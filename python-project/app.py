6a92789bbf36d2290cfd28c58374a39654aefeb3
sadik247 <sadik.syed247@gmail.com>
2026-05-10 23:39:10 +0530

    Added feature in feature-update branch

--- python-project/app.py
+++ python-project/app.py
@@ -3,4 +3,11 @@ print("Hello World")
 def greet(name):
     print(f"Hello, {name}")
 
-greet("Herovired")
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+print(add(5, 3))
+print(multiply(4, 2))
